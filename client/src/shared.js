0ff098917ee6e29c1f1cce42b88a6cd11692d616
Itenga <msukuma@protonmail.com>
2018-08-19 15:26:30 -0400

    using hard link instead of symlink to pass build stage

--- client/src/shared.js
+++ client/src/shared.js
@@ -1,1 +1,57 @@
-../../shared.js
+const uuidv5 = require('uuid/v5');
+const host = '0.0.0.0';
+const namespace = uuidv5(host, uuidv5.DNS);
+
+const { drones, droneIndex } = (n => {
+  const drones = [];
+  const droneIndex = {};
+  let id;
+  let key;
+
+  for (let i = 0; i < n; i++) {
+    id = 100000 + i;
+    key = uuidv5(id.toString(), namespace);
+    drones.push({
+      id,
+      key,
+      tracker: {
+        time: 0,
+        speed: 0,
+        moving: false,
+        max: 0,
+        anchor: null,
+      },
+    });
+
+    droneIndex[id] = i;
+  }
+
+  return { drones, droneIndex };
+})(10);
+
+function findDrone(id) {
+  const i = droneIndex[id];
+
+  return drones[i];
+}
+
+function stripDrone(drone) {
+  const {
+    id,
+    tracker: { speed, moving },
+  } = drone;
+
+  return { id, speed, moving };
+}
+
+module.exports = {
+  drones,
+  droneIndex,
+  findDrone,
+  stripDrone,
+  host,
+  webPort: 8000,
+  dronePort: 8001,
+  clientPort: 8002,
+  dronesPath: '/drones',
+};
