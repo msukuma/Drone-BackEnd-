b0a6555f15de39fc70c0aa22947a739e06189425
Itenga <msukuma@protonmail.com>
2018-08-19 11:37:54 -0400

    changed drone model and added helpers

--- shared.js
+++ shared.js
@@ -1,26 +1,55 @@
-const drones = (n => {
+const uuidv5 = require('uuid/v5');
+const host = '0.0.0.0';
+const namespace = uuidv5(host, uuidv5.DNS);
+
+const { drones, droneIndex } = (n => {
   const drones = [];
+  const droneIndex = {};
+  let id;
+  let key;
 
   for (let i = 0; i < n; i++) {
+    id = 100000 + i;
+    key = uuidv5(id.toString(), namespace);
     drones.push({
-      id: i,
-      name: `name-${i}`,
-      key: i,
-      moving: false,
+      id,
+      key,
       tracker: {
-        time: null,
-        locations: [],
+        time: 0,
+        speed: 0,
+        moving: false,
+        max: 0,
+        anchor: null,
       },
-      location: null,
     });
+
+    droneIndex[id] = i;
   }
 
-  return drones;
+  return { drones, droneIndex };
 })(10);
+
+function findDrone(id) {
+  const i = droneIndex[id];
+
+  return drones[i];
+}
+
+function stripDrone(drone) {
+  const {
+    id,
+    tracker: { speed, moving },
+  } = drone;
+
+  return { id, speed, moving };
+}
 
 module.exports = {
   drones,
-  host: '0.0.0.0',
+  droneIndex,
+  findDrone,
+  stripDrone,
+  host,
   webPort: 8000,
   dronePort: 8001,
   clientPort: 8002,
